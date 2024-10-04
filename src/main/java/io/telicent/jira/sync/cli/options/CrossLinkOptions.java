612ea36c832b9acaa6f63d778e8b12c8e1d01941
Rob Vesse <rob.vesse@telicent.io>
2024-10-04 10:18:17 +0100

    Upgrade to newer dependencies

--- src/main/java/io/telicent/jira/sync/cli/options/CrossLinkOptions.java
+++ src/main/java/io/telicent/jira/sync/cli/options/CrossLinkOptions.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import com.github.rvesse.airline.annotations.Option;
 import com.github.rvesse.airline.annotations.restrictions.Required;
 import io.telicent.jira.sync.client.model.CrossLinks;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +15,7 @@ import java.io.IOException;
  * Options relating to cross-links
  */
 public class CrossLinkOptions {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CrossLinkOptions.class);
 
     private final ObjectMapper mapper = new YAMLMapper();
 
@@ -35,6 +38,7 @@ public class CrossLinkOptions {
         }
 
         if (this.crossLinksFile.exists()) {
+            LOGGER.info("Loading existing cross links file {}", this.crossLinksFile.getAbsolutePath());
             this.instance = this.mapper.readValue(this.crossLinksFile, CrossLinks.class);
         } else {
             this.instance = new CrossLinks();
